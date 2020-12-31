50475a5cddfa669609bb8883fde6576939fa49ce
jmabranquinho <jmabranquinho@gmail.com>
2020-12-31 19:31:32 +0000

    Moved and improved speaker service

---
+++ Productivity++/Behaviors/Speaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Speech.Synthesis;
+
+namespace Productivity__
+{
+    public static class Speaker
+    {
+        private static SpeechSynthesizer _sp = GetSynth();
+        public static void Speak(string text)
+        {
+            if (_sp.State == SynthesizerState.Speaking)
+            {
+                _sp.SpeakAsyncCancelAll();
+            }
+
+            _sp.SpeakAsync(text);
+        }
+
+        private static SpeechSynthesizer GetSynth(int rate = 1)
+        {
+            var sp = new SpeechSynthesizer();
+            sp.Rate = rate;
+
+            return sp;
+        }
+    }
+}
